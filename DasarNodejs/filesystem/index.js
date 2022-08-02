ae8d1306ea81ba8a525463809ee8bbca2a43bf89
Abduh batubara <muhammadabdu089@gmail.com>
2022-08-02 14:35:13 +0700

    filesystem

---
+++ DasarNodejs/filesystem/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+
+const pathNotes = path.resolve(__dirname, "notes.txt");
+
+
+const data = fs.readFileSync(pathNotes, 'utf-8');
+console.log(data);
