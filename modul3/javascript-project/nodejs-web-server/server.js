fa0eb0412dc306917b45b1407b67936c7919907b
abduh batubara <muhammadabdu089@gmai.com>
2022-08-16 05:46:13 +0700

    Routing Request

--- modul3/javascript-project/nodejs-web-server/server.js
+++ modul3/javascript-project/nodejs-web-server/server.js
@@ -9,23 +9,39 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
     
+    // mendapatkan request method
     const { method } = request;
+    // mendapatkan request URL
+    const { url } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>')
-    }
-    if (method === 'POST') {
-        let body = [];
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end("Ini adalah homepage");
+        } else {
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+        }
+    } else if (url === '/about') { 
+        
+        if (method === "GET") {
+            response.end("Halo! ini adalah halaman about")
+        } else if (method === 'POST') {
+            let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        })
-    
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        })  
+            // mendapatkan request body
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            })
+        
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Hai, ${name}! ini adalah halaman about</h1>`);
+            })  
+        } else {
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+        }
+    } else {
+        response.end('Halaman tidak ditemukan!')
     }
 };
 
