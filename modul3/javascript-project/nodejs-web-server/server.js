fde1fc76d25ce051bd81e6bfa08215eda889d603
abduh batubara <muhammadabdu089@gmai.com>
2022-08-16 05:55:06 +0700

    Response Status

--- modul3/javascript-project/nodejs-web-server/server.js
+++ modul3/javascript-project/nodejs-web-server/server.js
@@ -16,13 +16,19 @@ const requestListener = (request, response) => {
 
     if (url === '/') {
         if (method === 'GET') {
+
+            response.statusCode = 200;
             response.end("Ini adalah homepage");
         } else {
+
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${method} request`)
         }
     } else if (url === '/about') { 
         
         if (method === "GET") {
+
+            response.statusCode = 200;
             response.end("Halo! ini adalah halaman about")
         } else if (method === 'POST') {
             let body = [];
@@ -35,12 +41,16 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+
+                response.statusCode = 200;
                 response.end(`<h1>Hai, ${name}! ini adalah halaman about</h1>`);
             })  
         } else {
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses dengan ${method} request`)
         }
     } else {
+        response.statusCode = 404;
         response.end('Halaman tidak ditemukan!')
     }
 };
